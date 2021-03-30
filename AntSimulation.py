c54046787afbcf5c60040010df7469417eee0c81
herecomesgibson <giol2710@colorado.edu>
2021-03-30 12:06:02 -0600

    Created initial python document for constructing the model

---
+++ AntSimulation.py
@@ -0,0 +1,14 @@
+#############################
+#
+# Dynamic models in Biology Project
+# Gibson Olbrys, Robert Gomez
+#
+#############################
+
+import numpy as np
+
+import math
+
+import matplotlib.pyplot as plt
+
+
